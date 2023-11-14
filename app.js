c8bf18c17b0d7ab87128656b2544cfddf332dce1
sikorskayaX <sikorskaya02@gmail.com>
2023-11-15 01:11:39 +0300

    Refactor aim training game:
    - Add random color generation for circles
    - Add additional time options (60s)
    - Update styling and colors to be more consistent
    - Move game logic functions into own functions
    - Add finish game functionality
    - Minor refactors and cleanup

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const screens = document.querySelectorAll('.screen')
 const timeList = document.querySelector('#time-list')
 const timeEl = document.querySelector('#time')
 const board = document.querySelector('#board')
+const colors = ['#7cb1e9', '#c8ddf5', '#c4dbf5', '#b6d6fa', '#5da2ec']
 
-let time = 10
+let time = 0
 let score = 0
 
 startBtn.addEventListener('click', (event) =>
@@ -31,13 +32,16 @@ board.addEventListener('click', event => {
     }
 })
 
-//debug
-startGame()
 
 function startGame(){
     createCircle()
     setInterval(decreaceTime, 1000)
     setTime(time)
+}
+
+function finishGame(){
+    timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1>Your score: <span class = "primary">${score}</span></h1>`
 }
 
 function decreaceTime(){
@@ -58,13 +62,16 @@ function setTime(value){
     timeEl.innerHTML = `00:${value}`
 }
 
+function getColor(){
+    const index = Math.floor(Math.random() * colors.length)
+    return colors[index]
+}
 
 function createCircle(){
     const circle = document.createElement('div')
-
     const size = getRandomNum(10, 50)
-
     const {width, height}= board.getBoundingClientRect()
+    const color = getColor()
 
     const x = getRandomNum(0, width - size)
     const y = getRandomNum(0, height - size)
@@ -74,6 +81,7 @@ function createCircle(){
     circle.style.height = `${size}px`
     circle.style.top = `${y}px`
     circle.style.left = `${x}px`
+    circle.style.backgroundColor = color
 
     board.append(circle)
 }
